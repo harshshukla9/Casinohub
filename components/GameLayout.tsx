aa79c3b5a679446daa61e66a6ee1f080280574ae
Fahad <fahad.khan2216@gmail.com>
2025-12-08 16:33:34 +0530

    added sounds

--- components/GameLayout.tsx
+++ components/GameLayout.tsx
@@ -11,19 +11,19 @@ export const GameLayout = () => {
       className="flex flex-col lg:flex-row w-full lg:gap-6 h-full lg:justify-between lg:w-full relative bg-[#1D1B1E] lg:p-2"
     >
       <div className="lg:flex lg:flex-col  lg:h-full lg:w-[35vw] lg:rounded-xl lg:overflow-hidden lg:gap-2 lg:bg-[#1F2326]">
-      {/* // 1 */}
+      {/* 1 */}
       <Header />
       <div className="px-4 hidden lg:block overflow-y-auto">
             <Controls />
           </div>
       </div>
+
       {/* 2 */}
         <div className="w-full">
           <GameBoard />
         </div>
 
         {/* 3 */}
-        {/* bg-[#1F2326] */}
           <div className="p-2 lg:hidden sm:p-4 bg-[#1F2326]">
             <Controls />
           </div>
